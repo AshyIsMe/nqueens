266c61f5a9add4eb0ab01b73ac7b2369a8aebad8
Aaron Ash <aaron.ash@gmail.com>
2021-09-23 21:28:46 +1000

    docs: clarify notes

--- nqueens/nqueens.py
+++ nqueens/nqueens.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 from random import randint
 
 
-# - model NxN board as nodes
-# - for each column place a queen and add edges to all squares she can reach
-#   - When placing a queen, she can only go on nodes with 0 edges
+# - Model NxN board as nodes in a graph.
+# - For each column place a queen and add edges to all squares she can reach.
+#   - When placing a queen, she can only go on nodes with 0 edges.
+# Currently randomly choosing a clear square for each column and
+# starting again if we find a column that's in no-mans-land.
 
 N = 8
 
